d9167fcde5210abdae439e7df2f05a2bcdd4e1ff
Creig Phiri <creig@kidoodle.tv>
2021-09-06 16:05:29 -0600

    fix failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hi there/i);
+  const linkElement = screen.getByText(/this is from/i);
   expect(linkElement).toBeInTheDocument();
 });
